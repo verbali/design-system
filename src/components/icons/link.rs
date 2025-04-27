80ab78da4481321d441cd51a39e203c3e0bbe361
Thomas Le Sciellour <thomas.lesciellour@observia-group.com>
2025-04-27 14:42:32 +0200

    add stroke properties on link and clipboard icon

--- src/components/icons/link.rs
+++ src/components/icons/link.rs
@@ -21,6 +21,8 @@ pub fn LinkIcon(props: LinkIconProps) -> Element {
             height: "{size}",
             fill: "none",
             path {
+                stroke_width: "2",
+                stroke_linecap: "round",
                 stroke: "{color}",
                 d: "M14 12C14 14.7614 11.7614 17 9 17H7C4.23858 17 2 14.7614 2 12C2 9.23858 4.23858 7 7 7H7.5M10 12C10 9.23858 12.2386 7 15 7H17C19.7614 7 22 9.23858 22 12C22 14.7614 19.7614 17 17 17H16.5"
             }
