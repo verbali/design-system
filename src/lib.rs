751b8c20648f77dbb22c3390d2dd9f96d2021c58
Thomas Le Sciellour <thomas.lesciellour@observia-group.com>
2025-04-27 02:26:29 +0200

    refactor assets module

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-use dioxus::prelude::*;
-
 pub mod components;
 
-pub const LOGO_SVG: Asset = asset!("/assets/logo.svg");
-pub const DS_CSS: Asset = asset!("/assets/ds.css");
+pub mod assets {
+    use dioxus::prelude::*;
+    pub const LOGO_SVG: Asset = asset!("/assets/logo.svg");
+    pub const DS_CSS: Asset = asset!("/assets/ds.css");
+}
