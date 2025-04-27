25ee47765dd657f469b85c359e5c16d9189dbb78
Thomas Le Sciellour <thomas.lesciellour@observia-group.com>
2025-04-27 02:37:57 +0200

    export fonts

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,10 @@ pub mod assets {
     use dioxus::prelude::*;
     pub const LOGO_SVG: Asset = asset!("/assets/logo.svg");
     pub const DS_CSS: Asset = asset!("/assets/ds.css");
+
+    pub mod fonts {
+        use dioxus::prelude::*;
+        pub const FONT_REGULAR: Asset = asset!("/assets/fonts/Fredoka-Regular.ttf");
+        pub const FONT_BOLD: Asset = asset!("/assets/fonts/Fredoka-SemiBold.ttf");
+    }
 }
